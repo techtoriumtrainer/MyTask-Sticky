eb6cffb03e41619cfb675944f074a76336711b66
techtoriumtrainer <trainer@techtorium.ac.nz>
2023-11-13 14:14:55 +1300

    Add project files.

---
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MauiApp1.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        Items.Add(Text);
+        Text= string.Empty;
+
+    }
+
+    [RelayCommand]  
+    
+    void Delete(string s) 
+    {
+        if(Items.Contains(s))
+        {  Items.Remove(s); }
+        
+    }
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
+    
+}
